bf191a13d6a5ebf92bb98fadde143b109e192e6f
Ben Haworth <ben@benhaworth.com>
2018-01-29 14:14:19 +0000

    Protocol all

--- oci_testing_benh/security_lists.tf
+++ oci_testing_benh/security_lists.tf
@@ -25,13 +25,13 @@ resource "oci_core_security_list" "ad2pub_seclist" {
   // allow outbound IP traffic
   egress_security_rules {
     destination = "0.0.0.0/0"
-    protocol = ""
+    protocol = "all"
   }
 
   // allow inbound IP traffic from Home IP
   ingress_security_rules {
     source = "80.229.19.7/32"
-    protocol = "" // ipv4
+    protocol = "all" // ipv4
   }
 
 }
