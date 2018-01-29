e78e1c6173703dc1936c006fa0126ac03c6515c5
Ben Haworth <ben@benhaworth.com>
2018-01-29 14:23:14 +0000

    AD3 & AD2

--- oci_testing_benh/security_lists.tf
+++ oci_testing_benh/security_lists.tf
@@ -17,6 +17,25 @@ provider "oci" {
 # Protocols are specified as protocol numbers.
 # http://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
 
+resource "oci_core_security_list" "ad2pub_seclist" {
+  compartment_id = "${var.compartment_ocid}"
+  vcn_id = "${var.vcn_ocid}"
+  display_name = "ad2pub"
+
+  // allow outbound IP traffic
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol = "all"
+  }
+
+  // allow inbound IP traffic from Home IP
+  ingress_security_rules {
+    source = "80.229.19.7/32"
+    protocol = "all" // ipv4
+  }
+
+}
+
 resource "oci_core_security_list" "ad3pub_seclist" {
   compartment_id = "${var.compartment_ocid}"
   vcn_id = "${var.vcn_ocid}"
